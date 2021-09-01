576b3e0235a3c57a5941a519c03c82cb2e37519f
Dhanwin2021 <83278381+Dhanwin2021@users.noreply.github.com>
2021-09-01 17:20:53 +0800

    Add files via upload

---
+++ MATH STUFF/gamepage.js
@@ -0,0 +1,87 @@
+player_name1 = localStorage.getItem("player1_name");
+player_name2 = localStorage.getItem("player2_name");
+
+
+
+player_score1 = 0;
+
+player_score2 = 0;
+
+question_turn="player_name1";
+answer_turn="player_name2";
+
+document.getElementById("player_name1").innerHTML= player_name1+";";
+
+document.getElementById("player_name2").innerHTML = player_name2+";";
+
+document.getElementById("player_score1").innerHTML = player_score1+";";
+
+document.getElementById("player_score2").innerHTML = player_score2+";";
+
+document.getElementById("player_question").innerHTML = "Player Who Asks - " + player_name1+";";
+
+document.getElementById("player_answer").innerHTML = "Player Who Answers - " + player_name2+";";
+
+
+
+function send() {
+    get_word = document.getElementById("word").value;
+    word = get_word.toLowerCase();
+    console.log("word in the lower case = "+word);
+    charAt1 = word.charAt(1);
+    console.log(charAt1);
+    
+    length_divide_2 = Math.floor(word.length/2);
+    charAt2 = word.charAt(length_divide_2);
+    console.log(charAt2);
+    
+    length_minus_1 = word.length-1;
+    charAt3 = word.charAt(length_minus_1);
+    console.log(charAt3);
+
+    remove_charAt1 = word.replace(charAt1,"_");
+    remove_charAt2 = remove_charAt1.replace(charAt2,"_");
+    remove_charAt3 = remove_charAt2.replace(charAt3,"_");
+    console.log(remove_charAt3);
+
+    question_word = "<h4 id='word_display'>Q."+remove_charAt3+"</h4>";
+    input_box = "<br>Answer:<input type='text' id='input_check_box'>";
+    check_button = "<br><br><button class='btn btn-primary' onclick='check()'>Check</button>";
+    row = question_word+input_box+check_button;
+    document.getElementById("output").innerHTML=row;
+    document.getElementById("word").value="";
+
+}
+
+function check(){
+    get_answer = document.getElementById("input_check_box").value;
+    answer = get_answer.toLowerCase();
+    console.log("answer in lower case - "+answer);
+    if(answer==word){
+        if(answer_turn=="player1"){
+            player_score1 = player_score1+1;
+            document.getElementById("player_score1").innerHTML=player_score1;
+        }
+        else{
+            player_score2 = player_score2+1;
+            document.getElementById("player_score2").innerHTML=player_score2;
+        }
+    }
+    if(question_turn=="player1"){
+        question_turn="player2";
+        document.getElementById("player_question").innerHTML="question_turn - "+player_name2;
+    }
+    else{
+        question_turn="player1"
+        document.getElementById("player_question").innerHTML="question_turn - "+player_name1;
+    }
+    if(answer_turn=="player1"){
+        answer_turn="player2";
+        document.getElementById("player_answer").innerHTML="answer_turn - "+player_name2;
+    }
+    else{
+        answer_turn="player1";
+        document.getElementById("player_answer").innerHTML="answer_turn - "+player_name1;
+    }
+    document.getElementById("output").innerHTML="";
+}
